2ccdcc298647e2f72376d50961a8dc34f73642a4
nishat-tonami <nishat.tonami121@gmail.com>
2025-03-25 16:29:55 +0600

    text added

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -6,7 +6,7 @@ public class TicTacToe {
     int boardWidth=600;
     int boardHeight=650;
     
-    JFrame frame = new JFrame();
+    JFrame frame = new JFrame("Tic-Tac-Toe");
     JLabel textLabel = new JLabel();
     JPanel textPanel = new JPanel();
 
@@ -18,8 +18,8 @@ public class TicTacToe {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
 
-        textLabel.setBackground(Color.white);
-        textLabel.setForeground(Color.blue);
+        textLabel.setBackground(Color.blue);
+        textLabel.setForeground(Color.white);
         textLabel.setFont(new Font("Arial",Font.BOLD,50));
         textLabel.setHorizontalAlignment(JLabel.CENTER);
         textLabel.setText("Tic-Tac-Toe");
@@ -28,5 +28,6 @@ public class TicTacToe {
         textPanel.setLayout(new BorderLayout());
         textPanel.add(textLabel);
         frame.add(textPanel);
+        frame.add(textPanel,BorderLayout.NORTH);
     }
 }
