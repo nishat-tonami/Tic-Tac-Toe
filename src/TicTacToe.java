225ea51fe6201fb4f0460cd586484d660a6883c7
nishat-tonami <nishat.tonami121@gmail.com>
2025-03-25 16:40:34 +0600

    Panel added

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -9,6 +9,12 @@ public class TicTacToe {
     JFrame frame = new JFrame("Tic-Tac-Toe");
     JLabel textLabel = new JLabel();
     JPanel textPanel = new JPanel();
+    JPanel boardPanel = new JPanel();
+
+    JButton [][] board = new JButton[3][3];
+    String playerX = "X";
+    String playerO = "O";
+    String currentPlayer = playerX;
 
     TicTacToe() {
         frame.setVisible(true);
@@ -18,7 +24,7 @@ public class TicTacToe {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
 
-        textLabel.setBackground(Color.blue);
+        textLabel.setBackground(Color.darkGray);
         textLabel.setForeground(Color.white);
         textLabel.setFont(new Font("Arial",Font.BOLD,50));
         textLabel.setHorizontalAlignment(JLabel.CENTER);
@@ -29,5 +35,18 @@ public class TicTacToe {
         textPanel.add(textLabel);
         frame.add(textPanel);
         frame.add(textPanel,BorderLayout.NORTH);
+
+        boardPanel.setLayout(new GridLayout(3,3));
+        boardPanel.setBackground(Color.darkGray);
+        frame.add(boardPanel);
+
+        for(int i=0;i<3;i++) {
+            for(int j=0;j<3;j++) {
+                JButton tile = new JButton();
+                board [i][j]=tile;
+                boardPanel.add(tile);
+            }
+        }
+
     }
 }
